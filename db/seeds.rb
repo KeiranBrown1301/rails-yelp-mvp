34a51e54af92f5b25c426a8b1f6f0a821d29305a
Keiran Brown <keiranbrown1301@gmail.com>
2022-01-11 21:35:54 +0900

    controller added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+puts 'cleaning up database'
+Restaurant.delete_all
+puts 'database is clean!'
+
+puts 'Creating seeds'
+5.times do
+  Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+  )
+end
+puts 'done!'
